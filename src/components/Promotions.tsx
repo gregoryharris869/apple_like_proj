31bbe130fa91ffc79a1c1050cac8616b6e89f743
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-09-16 00:48:04 -0500

    reset

--- src/components/Promotions.tsx
+++ src/components/Promotions.tsx
@@ -9,17 +9,17 @@ const Promotions = ({ promotions }: Props) => {
     <>
       {promotions.map(
         ({ title, price, description, buttonPromotion }, index) => (
-          <div className="space-y-8">
+          <div className="space-y-9">
             <div
               key={index}
               className="flex flex-col justify-between h-full space-y-4"
             >
-              <div className="space-y-4">
+              <div className="mb-5 space-y-4 ">
                 <h2 className="text-2xl font-medium">{title}</h2>
                 <h3 className="text-4xl font-bold">{price}</h3>
                 <p className="text-lg text-textGrey">{description}</p>
               </div>
-              <div className="flex justify-start">
+              <div className="flex justify-start ">
                 <Button size="medium">{buttonPromotion}</Button>
               </div>
             </div>
--- src/components/section/Pricing.tsx
+++ src/components/section/Pricing.tsx
@@ -8,50 +8,6 @@ const Pricing = () => {
       <div className="relative py-12 text-white ">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
           <Promotions promotions={promotionsData} />
-          {/* <div className="flex flex-col justify-between h-full space-y-4">
-            <div className="space-y-4">
-              <h2 className="text-2xl font-medium">Buy an Apple device</h2>
-              <h3 className="text-4xl font-bold">3 months free.</h3>
-              <p className="text-lg text-textGrey">
-                Apple TV+ is included for 3 months when you purchase an Apple
-                device and redeem the offer within 90 days.²
-              </p>
-            </div>
-            <div className="flex justify-start">
-              <Button size="medium">Check eligibility</Button>
-            </div>
-          </div>
-
-          <div className="flex flex-col justify-between h-full space-y-4">
-            <div className="space-y-4">
-              <h2 className="text-2xl font-medium">Free 7-day trial</h2>
-              <h3 className="text-4xl font-bold">$9.99/mo.</h3>
-              <p className="text-lg text-textGrey">
-                A monthly subscription is just $9.99 per month after a free
-                7-day trial.³ Share Apple TV+ with your family.⁴
-              </p>
-            </div>
-            <div className="flex justify-start">
-              <Button size="medium">Try it free</Button>
-            </div>
-          </div>
-
-          <div className="flex flex-col justify-between h-full space-y-4">
-            <div className="space-y-4">
-              <h2 className="text-2xl font-medium">Free 1-month trial</h2>
-              <h3 className="text-4xl font-bold">Apple One</h3>
-              <p className="text-lg text-textGrey">
-                Bundle Apple TV+ with up to five other great services for one
-                low monthly price. And enjoy more for less.
-              </p>
-              <a href="#" className=" hover:underline">
-                <span className="text-linkColor">Learn more &gt;</span>
-              </a>
-            </div>
-            <div className="flex justify-start">
-              <Button size="medium">Try Apple One free⁵</Button>
-            </div>
-          </div> */}
         </div>
       </div>
     </Container>
